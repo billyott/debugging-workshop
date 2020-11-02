88dc40befefdff22bc85989821e6cc80af0bbed8
billyott <willrichott@gmail.com>
2020-11-02 10:29:31 -0500

    Done

--- index.js
+++ index.js
@@ -2,26 +2,31 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
   const jokeList = document.getElementById('joke-list')
   const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
 
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    // .then(function(jokeData) {
+    //   return jokeData.joke
+    // })
   }
 
   form.addEventListener('submit', (event) => {
-
+    event.preventDefault()
+    const username = document.getElementById('name-input').value
     if(username === "") return;
     fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    .then(function(jokeData) {
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${jokeData.joke}
+      `
+      jokeList.appendChild(newJokeLi)
+    })
   })
+
 })
